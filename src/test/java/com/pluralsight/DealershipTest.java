7a2d7de6aa9020111e165b79720191955f4f866d
Nurbu <lamanurbu2@gmail.com>
2026-05-08 15:16:36 -0400

    Created basics null tests for all the Dealership methods except for getAllVehicles and addVehicle.

--- src/test/java/com/pluralsight/DealershipTest.java
+++ src/test/java/com/pluralsight/DealershipTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-import static org.testng.Assert.assertNull;
+import static org.testng.Assert.*;
 
 public class DealershipTest {
 
@@ -19,39 +19,143 @@ public class DealershipTest {
 
         Dealership dealership = new Dealership("Audi Meadowlands", "425 NJ-3, Secaucus, NJ 07094", "+18777014078");
         Vehicle a4 = new Vehicle(1021, 2026, "Audi", "A4", "Sedan", "Black", 15200, 35000);
-        dealership.addVehicle(a4);
 
         // Act
+
+        dealership.addVehicle(a4);
         List<Vehicle> vehicles = dealership.getVehiclesByPrice(3000.12, 50000.42);
+
         // Assert
+
         assertNull(vehicles);
     }
 
-//    @Test
-//    public void getVehiclesByMake() {
-//    }
-//
-//    @Test
-//    public void getVehiclesByYear() {
-//    }
-//
-//    @Test
-//    public void getVehiclesByColor() {
-//    }
-//
-//    @Test
-//    public void getVehiclesByMileage() {
-//    }
-//
-//    @Test
-//    public void getVehiclesByType() {
-//    }
-//
-//    @Test
-//    public void getAllVehicles() {
-//    }
-//
-//    @Test
-//    public void addVehicle() {
-//    }
+    @Test
+    public void getVehiclesByMake() {
+
+        // Arrange
+
+        Dealership dealership = new Dealership("Audi Meadowlands", "425 NJ-3, Secaucus, NJ 07094", "+18777014078");
+        Vehicle a4 = new Vehicle(1021, 2026, "Audi", "A4", "Sedan", "Black", 15200, 35000);
+
+        // Act
+
+        dealership.addVehicle(a4);
+        List<Vehicle> vehicles = dealership.getVehiclesByMake("Audi", "A4");
+
+        // Assert
+
+        assertNull(vehicles);
+
+
+    }
+
+    @Test
+    public void getVehiclesByYear() {
+        // Arrange
+
+        Dealership dealership = new Dealership("Audi Meadowlands", "425 NJ-3, Secaucus, NJ 07094", "+18777014078");
+        Vehicle a4 = new Vehicle(1021, 2026, "Audi", "A4", "Sedan", "Black", 15200, 35000);
+
+        // Act
+
+        dealership.addVehicle(a4);
+        List<Vehicle> vehicles = dealership.getVehiclesByYear(1995, 2026);
+
+        //Assert
+
+        assertNull(vehicles);
+    }
+
+
+    @Test
+    public void getVehiclesByColor() {
+
+        // Arrange
+
+        Dealership dealership = new Dealership("Audi Meadowlands", "425 NJ-3, Secaucus, NJ 07094", "+18777014078");
+        Vehicle a4 = new Vehicle(1021, 2026, "Audi", "A4", "Sedan", "Black", 15200, 35000);
+
+        // Act
+
+        dealership.addVehicle(a4);
+        List<Vehicle> vehicles = dealership.getVehiclesByColor("Black");
+
+        // Assert
+
+        assertNull(vehicles);
+
+    }
+
+    @Test
+    public void getVehiclesByMileage() {
+        // Arrange
+
+        Dealership dealership = new Dealership("Audi Meadowlands", "425 NJ-3, Secaucus, NJ 07094", "+18777014078");
+        Vehicle a4 = new Vehicle(1021, 2026, "Audi", "A4", "Sedan", "Black", 15200, 35000);
+
+        // Act
+
+        dealership.addVehicle(a4);
+        List<Vehicle> vehicles = dealership.getVehiclesByMileage(2313, 100000);
+
+        // Assert
+
+        assertNull(vehicles);
+
+    }
+
+    @Test
+    public void getVehiclesByType() {
+        // Arrange
+
+        Dealership dealership = new Dealership("Audi Meadowlands", "425 NJ-3, Secaucus, NJ 07094", "+18777014078");
+        Vehicle a4 = new Vehicle(1021, 2026, "Audi", "A4", "Sedan", "Black", 15200, 35000);
+
+        // Act
+
+        dealership.addVehicle(a4);
+        List<Vehicle> vehicles = dealership.getVehiclesByType("Sedan");
+
+        // Assert
+
+        assertNull(vehicles);
+
+    }
+
+    @Test
+    public void getAllVehicles() {
+        // Arrange
+
+        Dealership dealership = new Dealership("Audi Meadowlands", "425 NJ-3, Secaucus, NJ 07094", "+18777014078");
+        Vehicle a4 = new Vehicle(1021, 2026, "Audi", "A4", "Sedan", "Black", 15200, 35000);
+
+        // Act
+        dealership.addVehicle(a4);
+        List<Vehicle> vehicles = dealership.getAllVehicles();
+
+        // Assert
+
+        assertEquals(vehicles.size(), 1);
+    }
+
+    @Test
+    public void addVehicle() {
+        // Arrange
+
+        Dealership dealership = new Dealership("Audi Meadowlands", "425 NJ-3, Secaucus, NJ 07094", "+18777014078");
+        Vehicle a4 = new Vehicle(1021, 2026, "Audi", "A4", "Sedan", "Black", 15200, 35000);
+
+        // Act
+
+        dealership.addVehicle(a4);
+        List<Vehicle> vehicles = dealership.getAllVehicles();
+
+        // Assert
+        assertTrue(vehicles.contains(a4));
+        assertEquals(vehicles.size(), 1);
+
+
+    }
 }
+
