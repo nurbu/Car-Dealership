af2ed0e9db99e74ad64e9fdbe5c69f2d311f0492
Nurbu <lamanurbu2@gmail.com>
2026-05-08 13:57:33 -0400

    Added fields, constructor, getters and setters to Dealership class

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private List<Vehicle> vehicles;
+
+    public Dealership(ArrayList<Vehicle> vehicles, String name, String address, String phone) {
+        this.vehicles = vehicles;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
