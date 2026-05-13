e8e9a5c586175d6aa6918a21207830c1a5808462
Nurbu <lamanurbu2@gmail.com>
2026-05-13 03:03:37 -0400

    Changed Dealership dealership to private to only be handled by UserInterface class, got rid of the sout from addVehicle kept printing before the menu, and created UserInterface userInterface to test out the display function.

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -3,5 +3,7 @@ package com.pluralsight;
 public class Program {
     public static void main(String[] args) {
 
+        UserInterface userInterface = new UserInterface();
+        userInterface.display();
     }
 }
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class UserInterface {
 
-    Dealership dealership;
+    private Dealership dealership;
 
     public UserInterface() {
     }
